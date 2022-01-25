2cf2e248505e18d2169fcf14c631fe3069706304
Adrian Cantu <garbanyo@gmail.com>
2022-01-25 19:31:22 +0000

    added reticulate code review and made some effort in adding response class to features

--- features/join_epi_gen.R
+++ features/join_epi_gen.R
@@ -35,13 +35,43 @@ intSites_dd <- intSites %>% as.data.frame() %>%
   mutate(nn=NULL) %>%
   filter(seqnames %in% paste0("chr", c(1:22, "X", "Y", "M")))
 
-all_f <- left_join(gen_f,epi_f,by='GTPSposID')
-intSite_features <- left_join(intSites_dd,all_f,by='GTPSposID')
 
-saveRDS(intSite_features,file=file.path(.features_d,'intSite_feature.rds'))
 
 gtsp_avg_epi_gen <- left_join(gen_f,epi_f,by='GTPSposID') %>%
   separate(GTPSposID,c('GTSP','posID'),'(?<=GTSP....)(?=chr.*$)') %>%
   select(-posID) %>%
   group_by(GTSP) %>%
   summarise_all(mean, na.rm = TRUE)
+
+
+#import response data
+
+library(readxl)
+CARTSite_Response <- read_excel("CARTSite.Response.BOR.01142022.xlsx", 
+                     col_types = c("text", "numeric", "skip", 
+                     "date", "skip", "skip", "skip", "date", 
+                     "numeric", "text", "numeric", "text"))
+# BOR= best overal response
+colnames(CARTSite_Response) <- c('Trial','ID','inf_date','response_date','response_timepoint','BOR','BORc','note')
+dbConn  <- dbConnect(MySQL(), group='specimen_management')
+samples <- dbGetQuery(dbConn,'select * from gtsp where Trial="CART19_ALL" or Trial="CART19_CLL"')
+
+gtsp_to_pid <- samples %>% select(c("SpecimenAccNum","SamplePatientCode","Patient")) %>%
+  mutate(SamplePatientCode = str_replace(SamplePatientCode,'^CHP','CHOP')) %>%
+  separate(SamplePatientCode,c('Trial',NA),'_|\\-') %>%
+  mutate(Trial=ifelse(Trial=='xxx','CHP959',Trial)) %>%
+  separate(Patient,c(NA,'Patient_code'),'_|\\-') %>%
+  mutate(PID=paste0(Trial,'-',Patient_code)) %>%
+  mutate(GTSP=SpecimenAccNum) %>%
+  select(c(GTSP,PID))
+
+pid_to_bor <- CARTSite_Response %>%
+  mutate(PID=paste0(Trial,'-',sprintf("%02d", ID))) %>%
+  select(c(PID,BORc))
+ 
+gtsp_to_bor<- left_join(gtsp_to_pid,pid_to_bor,by='PID') 
+
+# all_f <- left_join(gen_f,epi_f,by='GTPSposID')
+# intSite_features <- left_join(intSites_dd,all_f,by='GTPSposID')
+# 
+# saveRDS(intSite_features,file=file.path(.features_d,'intSite_feature.rds'))
