52b6b1fc6189699ab3382ed90071df6be1ec97a9
Adrian Cantu <garbanyo@gmail.com>
2022-02-09 20:28:58 +0000

    cnage feature names and added missing files

--- features/join_epi_gen.R
+++ features/join_epi_gen.R
@@ -1,6 +1,9 @@
 library(tidyverse)
 library(RMySQL)
 library(hiAnnotator)
+library(vegan)
+library(reldist)
+#library(gintools)
 #library(furrr)
 
 gen_f <- readRDS(file.path(.features_d,'gen_data.rds'))
@@ -28,13 +31,20 @@ if( !file.exists(file.path(.data_d,'intSites_full_ALL_CLL.rds'))) {
 } else {
   intSites <- readRDS(file.path(.data_d,'intSites_full_ALL_CLL.rds'))
 }
+
 intSites_dd <- intSites %>% as.data.frame() %>%
   mutate(GTPSposID=paste0(GTSP,posid  )) %>%
-  group_by(GTSP) %>% mutate(nn=n()) %>%
-  ungroup() %>% filter(nn>100) %>%
-  mutate(nn=NULL) %>%
+#  group_by(GTSP) %>%
+#  mutate(nn=n()) %>%
+#  ungroup() %>%
+#  filter(nn>100) %>%
+#  mutate(nn=NULL) %>%
   filter(seqnames %in% paste0("chr", c(1:22, "X", "Y", "M")))
 
+# intSites_dd %>%
+#   filter(nn<=100) %>%
+#   pull(var=nn) %>%
+#   unique()
 
 
 gtsp_avg_epi_gen <- left_join(gen_f,epi_f,by='GTPSposID') %>%
@@ -54,24 +64,189 @@ CARTSite_Response <- read_excel("CARTSite.Response.BOR.01142022.xlsx",
 # BOR= best overal response
 colnames(CARTSite_Response) <- c('Trial','ID','inf_date','response_date','response_timepoint','BOR','BORc','note')
 dbConn  <- dbConnect(MySQL(), group='specimen_management')
-samples <- dbGetQuery(dbConn,'select * from gtsp where Trial="CART19_ALL" or Trial="CART19_CLL"')
 
-gtsp_to_pid <- samples %>% select(c("SpecimenAccNum","SamplePatientCode","Patient")) %>%
+########
+#samples <- dbGetQuery(dbConn,'select * from gtsp where Trial="CART19_ALL" or Trial="CART19_CLL"')
+samples <- dbGetQuery(dbConn,'select * from gtsp')%>%
+  filter(SpecimenAccNum %in% intSites$GTSP)
+
+
+gtsp_to_pid <- samples %>% select(c("SpecimenAccNum","SamplePatientCode","Patient","Trial")) %>%
   mutate(SamplePatientCode = str_replace(SamplePatientCode,'^CHP','CHOP')) %>%
-  separate(SamplePatientCode,c('Trial',NA),'_|\\-') %>%
-  mutate(Trial=ifelse(Trial=='xxx','CHP959',Trial)) %>%
-  separate(Patient,c(NA,'Patient_code'),'_|\\-') %>%
-  mutate(PID=paste0(Trial,'-',Patient_code)) %>%
+  separate(SamplePatientCode,c('Trial2',NA),'(?<=^(CHOP|UPCC))(?=\\d*(_|\\-))') %>%
+  mutate(Trial2=ifelse(Trial2=='xxx','CHOP',Trial2)) %>%
+  mutate(Patient=str_replace(Patient,'^\\D+','')) %>%
+  mutate(Patient=str_replace(Patient,'\\-?[^0-9\\-].*','')) %>%
+  mutate(PID=paste0(Trial2,Patient)) %>%
   mutate(GTSP=SpecimenAccNum) %>%
-  select(c(GTSP,PID))
+  select(c(GTSP,PID,Trial))
 
 pid_to_bor <- CARTSite_Response %>%
   mutate(PID=paste0(Trial,'-',sprintf("%02d", ID))) %>%
   select(c(PID,BORc))
  
-gtsp_to_bor<- left_join(gtsp_to_pid,pid_to_bor,by='PID') 
+gtsp_to_bor <- left_join(gtsp_to_pid,pid_to_bor,by='PID') 
 
-# all_f <- left_join(gen_f,epi_f,by='GTPSposID')
-# intSite_features <- left_join(intSites_dd,all_f,by='GTPSposID')
+# merging features and responses class
+
+features_responses <- left_join(gtsp_avg_epi_gen,gtsp_to_bor,by='GTSP')
+
 # 
-# saveRDS(intSite_features,file=file.path(.features_d,'intSite_feature.rds'))
+calculateUC50 <- function(abund){
+  stopifnot(is.vector(abund) & is.numeric(abund))
+  abund <- abund[order(abund)]
+  accum <- sapply(1:length(abund), function(i){sum(abund[1:i])})
+  length(accum[accum >= sum(abund)/2])
+}
+
+
+# Recreate columns used in published data.
+pop_stats <- intSites_dd %>%
+  mutate(timePoint=tolower(timePoint)) %>%
+  group_by(GTSP,timePoint,cellType) %>%
+#  mutate(within_gene=ifelse(nearest_geneDist == 0, TRUE, FALSE)) %>%
+  summarise(
+    "numUniqSites" = n(),
+    "estAbund_avg" = mean(estAbund),
+    "maxRelAbund" = max(relAbund),
+    "numClones" = sum(estAbund), 
+    "ShannonIndex" = diversity(estAbund),
+    "GiniIndex" = gini(estAbund),
+    "Chao1" = round(estimateR(estAbund, index='chao')[2], 0),
+    "UC50" = calculateUC50(estAbund),
+    "pctTxnUnit" = 100 * sum(inFeature, na.rm = TRUE)/n(),
+    "pctSameOrt" = 100 * sum(inFeatureSameOrt, na.rm = TRUE) / 
+      sum(inFeature, na.rm = TRUE),
+    "pctNearTxnUn" = 100 * sum(
+      inFeature == FALSE & abs(nearestFeatureDist) <= 5000, na.rm = TRUE) / 
+      sum(inFeature == FALSE, na.rm = TRUE),
+    "pctInOnco" = 100 * sum(abs(nearestOncoFeatureDist) <= 5000, na.rm = TRUE) / n(),
+    "nearest_geneDist" = mean(nearestFeatureDist),
+    "within_gene" = mean(ifelse(nearestFeatureDist == 0, 1, 0)),
+    "same_ort" = mean(inFeatureSameOrt),
+    .groups = 'drop'
+  )
+
+
+
+features_responses_pop <- left_join(features_responses,pop_stats,by='GTSP') %>%
+  relocate(c(PID,Trial,BORc),.after=GTSP)
+
+###
+# add cluster
+
+cluster_GTSP <- readRDS(file.path(.data_d,'cluster_GTSP.rds'))
+old_f_names <- c('GTSP'          , 'timePoint', 'PID'    , 'cellType', 'estAbund_avg')
+new_f_names <- c('SpecimenAccNum', 'Timepoint', 'Patient', 'CellType', 'estAbund')
+features_responses_pop2 <- left_join(features_responses_pop,cluster_GTSP,by='GTSP') %>%
+  rename_with(~ str_replace(.x,'Kb$','k')) %>%
+  rename_with(~ new_f_names, all_of(old_f_names)) %>%
+  mutate(Trial=ifelse(Trial=="Gill_CART19_18415","CART19_CLL",Trial)) %>%
+  relocate(c(Patient,Trial,Timepoint,CellType,BORc),.after=SpecimenAccNum)
+
+#save
+saveRDS(features_responses_pop2,file=file.path(.features_d,'sample_features_20220209.rds'))
+
+write.table(features_responses_pop2, file=file.path(.features_d,'sample_features_20220209.tsv'), quote=FALSE, sep='\t',row.names = FALSE)
+
+
+wb <- openxlsx::createWorkbook()
+openxlsx::addWorksheet(wb, "sample_features")
+openxlsx::writeDataTable(wb,"sample_features",features_responses_pop2)
+openxlsx::saveWorkbook(wb, file.path(.features_d,'sample_features_20220209.xlsx'), overwrite = TRUE)
+
+
+#######################################
+# 
+# missGTSP <- features_responses_pop %>%
+#   filter(is.na(BORc)) %>%
+#   pull(var=GTSP)
+
+#missSample <- 
+  
+#  samples %>%
+#  filter(SpecimenAccNum %in% missGTSP)
+
+#samples_miss <- dbGetQuery(dbConn,'select * from gtsp where Trial="CART19_ALL" or Trial="CART19_CLL"')
+#dbConn  <- dbConnect(MySQL(), group='specimen_management')
+#arr <-paste0("SELECT * FROM gtsp WHERE SpecimenAccNum IN ('",paste(missGTSP,collapse = "','"),"')")
+#samples_miss <- dbGetQuery(dbConn,arr)
+
+  
+#features_responses_pop$BORc
+library(readxl)
+John_CART <- read_excel("data/John_CART.xlsx")
+
+kk_j <- John_CART %>%
+  filter(!is.na(Chao1)) %>%
+  pull(var=Chao1,name=SpecimenAccNum)
+
+kk_a <- features_responses_pop2 %>%
+  pull(var=Chao1,name=SpecimenAccNum)
+
+blabla <- setdiff(names(kk_j),names(kk_a))
+int_blabla <- intersect(names(kk_j),names(kk_a))
+kk_aa <- kk_a[names(kk_a) %in% int_blabla]
+kk_jj <- kk_j[names(kk_j) %in% int_blabla]
+
+kk_aa[duplicated(names(kk_aa))]
+#kk_aaa <- 
+  
+features_responses_pop %>%
+  GenomicRanges::as.data.frame() %>%
+  dplyr::filter(GTSP=='GTSP0737') #%>%
+  nrow()
+
+  features_responses %>%
+    GenomicRanges::as.data.frame() %>%
+    dplyr::filter(GTSP=='GTSP0737') %>%
+  nrow()
+
+  # merging features and responses class
+  
+  features_responses <- left_join(gtsp_avg_epi_gen,gtsp_to_bor,by='GTSP')
+  
+  gtsp_avg_epi_gen %>%
+    GenomicRanges::as.data.frame() %>%
+    dplyr::filter(GTSP=='GTSP0737') %>%
+  nrow()
+  
+  gtsp_to_bor %>%
+    GenomicRanges::as.data.frame() %>%
+    dplyr::filter(GTSP=='GTSP0737') #%>%
+    nrow()
+    
+    gtsp_to_bor <- left_join(gtsp_to_pid,pid_to_bor,by='PID') 
+    
+    gtsp_to_pid %>%
+      dplyr::filter(GTSP=='GTSP0737')
+    
+    pid_to_bor %>%
+      dplyr::filter(PID=='UPCC04409-05')
+  
+sampless <- dbGetQuery(dbConn,'select * from gtsp')
+
+blabla2 <- sampless %>% filter(SpecimenAccNum %in% blabla)
+blabla3 <- intSites %>% as.data.frame() %>% filter(GTSP %in% blabla)
+blabla4 <- intSites_dd %>% filter(GTSP %in% blabla)
+
+unique(blabla2$SpecimenAccNum) %>% 
+  length()
+
+unique(blabla3$GTSP) %>% 
+  length()
+
+unique(blabla4$GTSP) %>% 
+  length()
+
+sum(features_responses_pop2$SpecimenAccNum %in% blabla)
+
+epikk <- epi_f %>% separate(GTPSposID,c('GTSP','posID'),'(?<=GTSP....)(?=chr.*$)')
+epikk %>% filter(GTSP %in% blabla) %>% nrow()
+
+genkk <- gen_f %>% separate(GTPSposID,c('GTSP','posID'),'(?<=GTSP....)(?=chr.*$)')
+genkk %>% filter(GTSP %in% blabla) %>% nrow()
+# colnames(features_responses_pop2)
+# colnames(John_CART)
+# setdiff(colnames(John_CART),colnames(features_responses_pop2))
+# intersect(colnames(John_CART),colnames(features_responses_pop2))
